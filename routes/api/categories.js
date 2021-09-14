1f609e3a4a9839539600b9939876498d2389ce57
imhamza1 <hamza.akram4635@gmail.com>
2021-09-14 21:21:04 +0500

    feat: category edit/delete api implemented

--- routes/api/categories.js
+++ routes/api/categories.js
@@ -10,10 +10,23 @@ router.get("/", async (req, res) => {
 });
 
 router.post("/", validateCategory, async (req, res) => {
-  console.log(req.body.category);
+  console.log(req.body.name);
   let category = new Category();
-  category.name = req.body.category;
+  category.name = req.body.name;
   await category.save();
+  res.send(category);
+});
+
+router.put("/:id", validateCategory, async (req, res) => {
+  console.log(req.body);
+  let category = await Category.findById(req.params.id);
+  category.name = req.body.name;
+  await category.save();
+  res.send(category);
+});
+
+router.delete("/:id", async (req, res) => {
+  let category = await Category.findByIdAndRemove(req.params.id);
   res.send(category);
 });
 
